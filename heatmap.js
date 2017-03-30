006e35a931b1c81602a8684c8743713f14f68d2e
John Clark <johngclark36@gmail.com>
2017-03-30 16:49:11 -0400

    Create scales for the x axis and y axis, have the x axis spaced out 10 years apart

--- heatmap.js
+++ heatmap.js
@@ -7,14 +7,51 @@ $("document").ready(function () {
     console.log(result)
     var baseTemperature = result.baseTemperature;
 
-    var margin = {  top: 80, right: 140, bottom: 60, left: 60 }
-    var width = 900 - margin.left - margin.right
+    var margin = {  top: 80, right: 140, bottom: 60, left: 100 }
+    var width = 1300 - margin.left - margin.right
     var height = 700 - margin.top - margin.bottom
     var svg = d3.select("body").append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+    //set up x and y scales
+
+    var xScale = d3.time.scale()
+      .domain([new Date("1753"), new Date("2015")])
+      .range([0, width])
+
+    var yScale = d3.scale.ordinal()
+      .domain(["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"])
+      .rangePoints([0, height])
+
+    var xAxis = d3.svg.axis()
+      .scale(xScale)
+      .orient("bottom")
+      .ticks(d3.time.year, 10)
+
+
+    var yAxis = d3.svg.axis()
+      .scale(yScale)
+      .orient("left")
+      .ticks(12, "")
+
+      svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.8em")
+      .style("text-anchor", "middle")
+      .text("Months");
+
 
 
   })
