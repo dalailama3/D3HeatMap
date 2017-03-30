72c543a5f53024f668a31b9fdf3a5b90f7ed132d
John Clark <johngclark36@gmail.com>
2017-03-30 15:50:56 -0400

    Use jquery ajax function to retrieve the json data, append an svg element to the dom with d3 with height and width

--- heatmap.js
+++ heatmap.js
@@ -1,3 +1,22 @@
 $("document").ready(function () {
-  
+
+  $.ajax({
+    url: "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json",
+    dataType: "json"
+  }).done(function (result) {
+    console.log(result)
+    var baseTemperature = result.baseTemperature;
+
+    var margin = {  top: 80, right: 140, bottom: 60, left: 60 }
+    var width = 900 - margin.left - margin.right
+    var height = 700 - margin.top - margin.bottom
+    var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+
+  })
+
 });
